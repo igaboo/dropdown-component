9ed262a60c3d5fa007d499591c42ebf081c41686
igaboo <dw.jaden@gmail.com>
2022-04-29 16:34:43 -0700

    new home for dropdown components

---
+++ src/react-dropdown.js
@@ -0,0 +1,176 @@
+import "./dropdown.scss";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useRef } from "react";
+
+const Button = ({ text, icon, color, func, iconsOnly, textOnly }) => {
+  const changeBackground = (e) => {
+    !color
+      ? (e.target.style.backgroundColor = "var(--hover")
+      : (e.target.style.backgroundColor = `rgba(${color}, .2)`);
+  };
+  const removeBackground = (e) => {
+    e.target.style.backgroundColor = "transparent";
+  };
+
+  return (
+    <button
+      style={{
+        color: `rgba(${color}, 1)`,
+        justifyContent: `${textOnly || iconsOnly ? "center" : "space-between"}`,
+      }}
+      onMouseEnter={(e) => changeBackground(e)}
+      onMouseLeave={(e) => removeBackground(e)}
+      onClick={func}
+    >
+      {!textOnly && (
+        <FontAwesomeIcon
+          style={{ color: `rgba(${color}, 1)` }}
+          icon={icon}
+          size="lg"
+        />
+      )}
+      {!iconsOnly && text}
+    </button>
+  );
+};
+
+function DropdownMenu({
+  buttons,
+  closeMenu,
+  collapsed,
+  position,
+  iconsOnly,
+  textOnly,
+}) {
+  function useOutsideAlerter(ref) {
+    useEffect(() => {
+      function handleClickOutside(event) {
+        !collapsed && closeMenu();
+      }
+
+      document.addEventListener("mouseup", handleClickOutside);
+      return () => {
+        document.removeEventListener("mouseup", handleClickOutside);
+      };
+    }, [collapsed]);
+  }
+
+  let styles = {
+    transformOrigin: "50% 50%",
+  };
+
+  if (position === "top-left") {
+    styles = {
+      transformOrigin: "0% 0%",
+      left: "-50%",
+      marginTop: ".75rem",
+    };
+  } else if (position === "top-right") {
+    styles = {
+      transformOrigin: "100% 0%",
+      right: "-50%",
+      marginTop: ".75rem",
+    };
+  } else if (position === "top-center") {
+    styles = {
+      transformOrigin: "50% 0%",
+      transform: "translate(-50%)",
+      left: "50%",
+      marginTop: ".75rem",
+    };
+  } else if (position === "bottom-left") {
+    styles = {
+      transformOrigin: "0% 100%",
+      left: "-50%",
+      top: "100%",
+      transform: "translate(0, calc(-100% + -2rem))",
+      marginTop: "-0.25rem",
+    };
+  } else if (position === "bottom-center") {
+    styles = {
+      transformOrigin: "50% 100%",
+      left: "50%",
+      top: "100%",
+      transform: "translate(-50%, calc(-100% + -2rem))",
+      marginTop: "-0.25rem",
+    };
+  } else if (position === "bottom-right") {
+    styles = {
+      transformOrigin: "100% 0%",
+      right: "-50%",
+      top: "100%",
+      transform: "translate(0, calc(-100% + -2rem))",
+      marginTop: "-0.25rem",
+    };
+  }
+
+  if (iconsOnly || textOnly) {
+    styles["width"] = "auto";
+    styles["alignItems"] = "center";
+  } else {
+    styles["width"] = "8rem";
+  }
+
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef);
+
+  return (
+    <div className={`wrapper ${!collapsed ? "activated" : "deactivated"}`}>
+      <div
+        ref={wrapperRef}
+        className={`dropdown ${collapsed ? "collapsed" : "expanded"}`}
+        style={styles}
+      >
+        {buttons.map((btn) => {
+          return (
+            <Button
+              key={buttons.indexOf(btn)}
+              text={btn.text}
+              icon={btn.icon}
+              color={btn.color}
+              func={btn.func}
+              iconsOnly={iconsOnly}
+              textOnly={textOnly}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default function Dropdown({
+  state,
+  menu,
+  icon,
+  openMenu,
+  closeMenu,
+  buttons,
+  collapsed,
+  position,
+  iconsOnly,
+  textOnly,
+}) {
+  return (
+    <div className="dropdown-wrapper">
+      <FontAwesomeIcon
+        icon={icon}
+        size="lg"
+        style={{
+          color: !state && "var(--active)",
+        }}
+        className="btn-icon"
+        onClick={() => openMenu(menu)}
+      />
+      <DropdownMenu
+        buttons={buttons}
+        closeMenu={closeMenu}
+        collapsed={collapsed}
+        position={position}
+        iconsOnly={iconsOnly}
+        textOnly={textOnly}
+      />
+    </div>
+  );
+}
